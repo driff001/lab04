af27e3a8d668e970dfa49776a7d66be6712edfe4
Dylan Riffel <driff001@wch136-24.cs.ucr.edu>
2021-04-15 19:02:03 -0700

    Completed Part1

---
+++ driff001_lab4_part1.c
@@ -0,0 +1,84 @@
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+
+enum STATES { S_Wait, S_Press1, S_Press2} S_State;
+void Tick(){
+   switch(S_State) //transistions
+   {
+    
+   
+   
+   case SM1_Wait: //wait state
+   if(!A0){
+      S_State = S_Wait;
+   }
+   else if( ((PINA & 0b01) == 1)  && (PORTB == 0b01) ){
+        
+        S_State = S_Press1;
+   }
+   else if( ((PINA & 0b01) == 1)  && (PORTB == 0b10) ){
+        S_State = S_Press2;
+   }   
+   break;
+   
+   case S_Press1: //increment state
+   if(!A0){
+   S_State = S_Wait;
+   }
+   else{
+   S_State = S_Press1;  
+   }
+   break;
+   
+   case S_Press2: //decrement state
+   if(!A0){
+   S_State = S_Wait;
+   }
+   else{
+   S_State = S_Press2;  
+   }
+   break;
+   
+   default:
+   break;
+   }
+   
+   switch(S_State) //State actions  
+   {
+      case S_Press1:
+      PORTB = 0b10;
+      break;
+      
+      case S_Wait: //wait state
+      break;
+      
+      case S_Press2: //incrment state
+      PORTB = 0b01;
+      break;
+           
+      default:
+      break;
+
+   }
+}
+
+
+
+int main(void) {
+S_State = S_Wait;
+Tick();
+
+
+
+
+
+
+
+
+
+return 0;
+}
