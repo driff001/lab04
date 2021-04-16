25e4df0f01ce8dc8f050935cc22f0a71c9c03ec6
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-16 15:45:47 -0700

    Update driff001_lab4_part2.c

--- driff001_lab4_part2.c
+++ driff001_lab4_part2.c
@@ -23,7 +23,7 @@ void Tick(){
    else if( PINA == 0b10  ){
         S_State = S_Press2;
    }
-   else{
+   else if ( PINA == 0b11){
    	S_State = S_Press3;
    }  
    break;
@@ -89,9 +89,7 @@ void Tick(){
       break;
       
       case S_Press3: //incrment state
-      if (PINA == 0b11){
-      PORTC = 0b00;
-      }
+      PORTC = 0x00;
       break; 
       case S_PressWait:
       break;
