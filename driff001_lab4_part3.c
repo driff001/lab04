58af6facdfea899fb1370557593eade11326e30c
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-16 00:05:37 -0700

    Update driff001_lab4_part3.c

--- driff001_lab4_part3.c
+++ driff001_lab4_part3.c
@@ -20,17 +20,14 @@ void Tick(){
    S_State = S_Lock;
    }
 
-   if( (PINA & 0b100) == 1 ){
+   if( (PINA & 0b100) == 4 ){
         
         S_State = S_Press1;
    }
-   else if( ((PINA & 0b10) == 1)  && (count == 1) ){
-	printf("bruh");
+   else if( ((PINA & 0b10) == 2)  ){
         S_State = S_Press2;
    }
-   else if(PORTB == 0b01){  
-   S_State = S_Press2;
-   }
+
    else {
         S_State = S_Lock;
    }   
