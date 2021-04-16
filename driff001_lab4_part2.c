f5a017dccd8be0d4e13c0dd83a2e9be0ef81dc85
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-15 22:24:23 -0700

    Update driff001_lab4_part2.c

--- driff001_lab4_part2.c
+++ driff001_lab4_part2.c
@@ -19,7 +19,7 @@ void Tick(){
         
         S_State = S_Press1;
    }
-   if( ((PINA & 0b10) == 1)  && (PORTC > 0) ){
+   if( ((PINA & 0b10) == 2)  && (PORTC > 0) ){
         S_State = S_Press2;
    }
    else if( (PINA & 0b11) == 3 ){
