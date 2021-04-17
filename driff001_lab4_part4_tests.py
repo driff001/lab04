3e60310bd9d38f88e682d3e5d38206586d8c2916
driff001 <74107735+driff001@users.noreply.github.com>
2021-04-16 19:56:20 -0700

    Completed Part 4

---
+++ driff001_lab4_part4_tests.py
@@ -0,0 +1,14 @@
+  
+tests = [ {'description': 'PINA: 0x00,PINB: 0x00 0x00 => PORTC: 0x00',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x00)],
+    },
+    {'description': 'PINA: 0x0F,PINB: 0x00 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x04)],
+    },
+    {'description': 'PINA: 0x0F,PINB: 0xF0 => PORTB: 0x08',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x08)],
+    },
+    ]
